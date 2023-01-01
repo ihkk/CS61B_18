fb3a2d9235513ecacac196c56ea0812950535fd5
Yuki <ihkk.nb@gmail.com>
2023-01-01 13:17:48 +0800

    update hw0

---
+++ hw0/c0.java
@@ -0,0 +1,13 @@
+public class c0 {
+    public static void main(String[] args) {
+        int x = 5;
+
+        if (x < 10)
+            x = x + 10;
+
+        if (x < 10)
+            x = x + 10;
+
+        System.out.println(x);
+    }
+}
